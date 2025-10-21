06557591ff0fa8d8be9f2f1e91e1949a84bc1af7
bhuvanesh-km <bhuvaneshkm441@gmail.com>
2025-10-21 22:59:41 +0530

    initial commit with landing screen

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import { Terminal } from "./terminal";
+import { BackgroundRippleEffect } from "@/components/ui/background-ripple-effect";
+
+export default function Home() {
+  return (
+    <div className="relative flex min-h-screen w-full flex-col items-center justify-center overflow-hidden">
+      <BackgroundRippleEffect rows={14} cols={35} cellSize={56} />
+      <Terminal />
+    </div>
+  );
+}
---
+++ src/app/(home)/terminal.tsx
@@ -0,0 +1,3 @@
+export const Terminal = () => {
+  return <div className="relative z-10 mx-4 max-w-4xl">Terminal Component</div>;
+};
