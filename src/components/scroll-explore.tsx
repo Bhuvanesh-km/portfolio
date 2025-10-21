f78330735b74f8eb1143180acc6b23be56877d68
bhuvanesh-km <bhuvaneshkm441@gmail.com>
2025-10-22 00:10:37 +0530

    fix scroll

--- src/components/scroll-explore.tsx
+++ src/components/scroll-explore.tsx
@@ -1,10 +1,23 @@
+"use client";
+
 import { ArrowDown } from "lucide-react";
+import { Button } from "./ui/button";
 
 const ScrollExplore = () => {
+  const handleScroll = () => {
+    console.log(window.innerHeight, " scrolling");
+    window.scrollTo({
+      top: window.innerHeight,
+      behavior: "smooth",
+    });
+  };
   return (
-    <div className="text-muted-foreground absolute bottom-4 w-full animate-bounce text-center text-sm">
+    <div
+      onClick={handleScroll}
+      className="text-muted-foreground absolute bottom-4 z-20 w-full animate-bounce text-center text-sm"
+    >
       <h2>Scroll To Explore</h2>
-      <ArrowDown className="mx-auto mt-1 size-4" />
+      <ArrowDown className="mx-auto mt-1 size-4 cursor-pointer" />
     </div>
   );
 };
